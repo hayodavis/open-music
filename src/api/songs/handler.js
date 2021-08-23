daca085edfe330e974d4f7ef52b36de404dfb095
Davis Nur Iqbal Rifandi <davis.fandi@gmail.com>
2021-08-23 23:35:17 +0700

    sukses tes postman open music api v1 dicoding

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -12,14 +12,14 @@ class SongsHandler {
     this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
   }
 
-  postSongHandler(request, h) {
+  async postSongHandler(request, h) {
     try {
-      this._validator.validateSongPayload(Request.payload);
+      this._validator.validateSongPayload(request.payload);
       const {
         title = 'untitled', year, performer, genre, duration,
       } = request.payload;
 
-      const songId = this._service.addSong({
+      const songId = await this._service.addSong({
         title, year, performer, genre, duration,
       });
 
@@ -52,8 +52,8 @@ class SongsHandler {
     }
   }
 
-  getSongsHandler() {
-    const songs = this._service.getSongs();
+  async getSongsHandler() {
+    const songs = await this._service.getSongs();
     return {
       status: 'success',
       data: {
@@ -62,10 +62,10 @@ class SongsHandler {
     };
   }
 
-  getSongByIdHandler(request, h) {
+  async getSongByIdHandler(request, h) {
     try {
       const { id } = request.params;
-      const song = this._service.getSongById(id);
+      const song = await this._service.getSongById(id);
       return {
         status: 'success',
         data: {
@@ -92,12 +92,12 @@ class SongsHandler {
     }
   }
 
-  putSongByIdHandler(request, h) {
+  async putSongByIdHandler(request, h) {
     try {
-      this._validator.validateSongPayload(Request.payload);
+      this._validator.validateSongPayload(request.payload);
       const { id } = request.params;
 
-      this._service.editSongById(id, request.payload);
+      await this._service.editSongById(id, request.payload);
 
       return {
         status: 'success',
@@ -123,10 +123,10 @@ class SongsHandler {
     }
   }
 
-  deleteSongByIdHandler(request, h) {
+  async deleteSongByIdHandler(request, h) {
     try {
       const { id } = request.params;
-      this._service.deleteSongById(id);
+      await this._service.deleteSongById(id);
 
       return {
         status: 'success',
